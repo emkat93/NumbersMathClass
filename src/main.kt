8e19b7a23a852487f38d464c809656bb51555ceb
emkat93 <eallen5064@students.mchenry.edu>
2020-09-15 11:07:27 -0500

    Added new code and new comments

--- src/main.kt
+++ src/main.kt
@@ -2,9 +2,18 @@ import java.text.DecimalFormat
 
 fun main(){
 
-    println("Please enter the cost of bill.")  // ask the user a question
+    var userAns = 0.00  // variable that will hold the user input
+    var cost = 0.00  // variabe that will hold the total cost of the user's items
 
-       var cost = readLine()!!.toDouble() // ask the user to enter the amount of their bill
+    println("Please enter the price of your items.")  // ask the user a question
+    println("Enter 0.00 to end") // let's the user know how to stop putting more inputs
+    do{
+        userAns = readLine()!!.toDouble()    // ask the user to enter the amount of their bill
+        if (userAns != 0.00){ // checks to see if the user input doesn't equal 0.00
+            cost += userAns  // adds the user input to the cost variable to hold multiple inputs
+        }
+    }while (0.00 != userAns) // checks to see if user input doesn't equal 0.00
+
         calculatedCost(cost) // carries the cost to the next function
 
 }
@@ -19,5 +28,5 @@ fun calculatedCost(cost: Double) {  // the next function will calculate the tax 
     var totalCost = ((taxCost * tip) + taxCost) // calculates the the total cost with tip
     var formatted = twoDigits.format(totalCost)  // formats the total cost
 
-    println("Your total is $$formatted")  // prints the results 
+    println("Your total is $$formatted")  // prints the results
 }
